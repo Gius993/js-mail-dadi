959bfe93f96ba3adae3e45608a79bdb70074bbb5
Gius993 <giuseppevidili9@gmail.com>
2022-06-08 16:12:06 +0200

    generatore numero

---
+++ Gioco dadi/js/script.js
@@ -0,0 +1,7 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+for(let i = 1; i <= 6; i++){
+	console.log(i)
+}
